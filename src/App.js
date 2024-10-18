11e0e38b218782a0f0b3e2c24a81d7be6def599a
samirtamang33 <152047704+samirtamang33@users.noreply.github.com>
2024-10-18 19:16:58 +0545

    add SearchBar Components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 
 import BusinessList from "../src/components/BusinessList/BusinessList";
+import SearchBar from "./components/SearchBar/SearchBar";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
+        <SearchBar />
         <BusinessList />
       </div>
     );
